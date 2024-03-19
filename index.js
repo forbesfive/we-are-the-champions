af765b90e391e0704fa7608bac54870a2b564fc7
forbesfive <forbesfive@hotmail.com>
2024-03-19 10:29:39 +0000

    Added 3 of 4 Stretch goals
    Add From and To input fields
    Reverse the order of enddorsements
    Add likes with click to increase

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // javascript
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove, set} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-7b31c-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -13,23 +13,33 @@ const endorsementListInDB = ref(database, "endorsementList")
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const endorsementListEL = document.getElementById("endorsement-list")
+const fromInputFieldEl = document.getElementById("from-input-field")
+const toInputFieldEl = document.getElementById("to-input-field")
 
 addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
-    
+    if (inputFieldEl.value === "" || fromInputFieldEl.value === "" || toInputFieldEl.value === "") {
+        alert("Please complete all fields")
+    } else {
+    let inputValue = {
+        to: toInputFieldEl.value,
+        endorsement: inputFieldEl.value, 
+        from: fromInputFieldEl.value,
+        likes: 0}
+           
     push(endorsementListInDB, inputValue)
     
-    clearInputFieldEL()
+    clearInputFields()   
+    }
 } )
 
 onValue(endorsementListInDB, function(snapshot){
     if(snapshot.exists()) {
         let itemsArray = Object.entries(snapshot.val())
-        
         clearEndorsementListEL()
         
-        for (let i = 0; i < itemsArray.length; i++) {
+        for (let i = itemsArray.length - 1; i >= 0; i--) {
             let currentItem = itemsArray[i]
+            
             appendItemtoEndorsementListEL(currentItem)
         } 
     } else {
@@ -37,8 +47,10 @@ onValue(endorsementListInDB, function(snapshot){
     }
 })
 
-function clearInputFieldEL() {
-    inputFieldEl.value = ""   
+function clearInputFields() {
+    inputFieldEl.value = ""
+    fromInputFieldEl.value = ""
+    toInputFieldEl.value = ""  
 }
 
 function clearEndorsementListEL() {
@@ -47,15 +59,28 @@ function clearEndorsementListEL() {
 
 function appendItemtoEndorsementListEL(item) {
     let itemId = item[0]
-    let itemValue = item[1]
+    let itemValue = `
+        <p>To ${item[1].to}</p>
+        ${item[1].endorsement}
+        <div id="likes-container">
+            <p>From ${item[1].from}</p>
+            <p>🖤${item[1].likes}</p>
+        </div>`
     
     let newEl = document.createElement("li")
-    newEl.textContent = itemValue
+    newEl.innerHTML = itemValue
     
-    newEl.addEventListener("dblclick", function() {
+    newEl.addEventListener("click", function() {
         let locationOfItemInDb = ref(database, `endorsementList/${itemId}`)
         
-        remove(locationOfItemInDb)
+        let likesCount = item[1].likes + 1
+        
+        set(locationOfItemInDb, {
+            to: item[1].to,
+            endorsement: item[1].endorsement, 
+            from: [item[1].from],
+            likes: likesCount}
+            )
     })
     
     endorsementListEL.append(newEl)
